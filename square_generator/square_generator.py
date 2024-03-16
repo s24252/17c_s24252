2bcaf219f42fffe61c2caec9998044f467549b49
Adilet <adiletjeenkulov@gmail.com>
2024-03-16 18:32:55 +0100

    TASK 10

--- square_generator/square_generator.py
+++ square_generator/square_generator.py
@@ -1,11 +1,11 @@
 import math
 
-class SquareGenerator:
+from abc import ABC, abstractmethod
+
+class SquareGenerator(ABC):
+    @abstractmethod
     def generate_squares(self, start, end):
-        if end < start:
-            start, end = end, start
-        squares = [x**2 for x in range(start, end + 1)]
-        return squares
+        pass
 
     def calculate_square_roots(self, numbers):
         square_roots = [math.sqrt(num) for num in numbers]
