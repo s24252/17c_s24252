361d2dfc19f5f15888e93ecb86af9431e691dc17
Adilet <adiletjeenkulov@gmail.com>
2024-03-16 18:07:35 +0100

    TASK 2

--- main.py
+++ main.py
@@ -2,3 +2,13 @@
 squares = [x**2 for x in range(1,10)]
 
 print(squares)
+
+#Expand the previous program by defining a function that takes a range of numbers as input
+# and returns a list of squares for that range.e_squares(start, end))
+def generate_squares(start, end):
+    squares = [x**2 for x in range(start, end + 1)]
+    return squares
+start_num = 1
+end_num = 10
+result = generate_squares(start_num, end_num)
+print(f"Squares of numbers from {start_num} to {end_num}: {result}")
